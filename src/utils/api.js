6c68db5be95bc6277212349c80405a1c1aefddc9
Andy Smith <andy151993@hotmail.com>
2021-10-19 11:57:52 +0100

    destructure args

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,11 +4,17 @@ const news = axios.create({
   baseURL: "https://as-news-articles.herokuapp.com/api",
 });
 
-export const getArticles = async (page) => {
+export const getArticles = async ({ page, topic }) => {
   const { data } = await news.get("/articles", {
     params: {
       p: page,
+      topic: topic,
     },
   });
   return data;
+};
+
+export const getTopics = async () => {
+  const { data } = await news.get("/topics");
+  return data.topics;
 };
