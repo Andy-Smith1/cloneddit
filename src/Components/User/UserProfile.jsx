42f5395ead995d154a164a9290ed648c3e6007d3
Andy Smith <andy151993@hotmail.com>
2021-10-22 14:39:08 +0100

    avatar url validation and error handling

--- src/Components/User/UserProfile.jsx
+++ src/Components/User/UserProfile.jsx
@@ -3,17 +3,25 @@ import { UserContext } from "../../contexts/UserContext";
 import { changeAvatar } from "../../utils/api";
 import "../../Styles/UserProfile.scss";
 import { Link } from "react-router-dom";
+import { regex } from "../../utils/format";
 
 const UserProfile = () => {
   const { userLogin, setUserLogin } = useContext(UserContext);
   const [newAvatarUrl, setNewAvatarUrl] = useState(userLogin.avatar_url);
+  const [errorString, setErrorString] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleAvatarChange = (e) => {
     e.preventDefault();
+    setErrorString("");
+    if (!regex.test(newAvatarUrl)) {
+      setErrorString("Invalid Avatar URL");
+      return;
+    }
+    setIsLoading(true);
     changeAvatar(userLogin.username, newAvatarUrl).then((userFromApi) => {
-      setUserLogin(() => {
-        return { loggedIn: true, user: userFromApi };
-      });
+      setUserLogin(userFromApi);
+      setIsLoading(false);
     });
   };
 
@@ -27,7 +35,11 @@ const UserProfile = () => {
         Go Home
       </Link>
       <section className="profile-card">
-        <img src={userLogin.avatar_url} alt="avatar" />
+        {isLoading ? (
+          <p className="loading">Loading</p>
+        ) : (
+          <img src={userLogin.avatar_url} alt="avatar" />
+        )}
         <div>
           <h2>{userLogin.username}</h2>
           <h4>{userLogin.name}</h4>
@@ -40,6 +52,7 @@ const UserProfile = () => {
             />
             <button>Change</button>
           </form>
+          {errorString !== "" && <p>{errorString}</p>}
           <button className="logout-btn" onClick={handleLogOut}>
             Log Out
           </button>
